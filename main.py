a89472a015a2cf0e0de11fa223131a8bca510779
david <kolard@jirovcovka.net>
2022-10-09 13:04:45 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_menu
 from time import time
+from collections import deque
 from pygame.locals import *
 from random import choice, randint
 from pygame_menu.locals import *
@@ -23,14 +24,16 @@ class Lukas():
         self.x = x
         self.y = y
         self.velocity = 0
+        self.x_velocity = 10
         self.timer_animation = Timer()
         self.timer_move = Timer()
-        self.animation_delay = 200
+        self.default_animation_delays = [400, 200, 10]
+        self.animation_delay = self.default_animation_delays[1]
         self.move_delay = 10
         self.timer_animation.set(self.animation_delay)
         self.pozice = 0
-        self.change = 0.7
-        self.jump_velocity = -18
+        self.change = 1
+        self.jump_velocity = -20
         img1 = pygame.image.load("sprites/lukas_tycka01.png")
         img2 = pygame.image.load("sprites/lukas_tycka02.png")
         self.animations = [pygame.transform.scale(img1, (img1.get_width()*3, img1.get_height()*3)), pygame.transform.scale(img2, (img2.get_width()*3, img2.get_height()*3))]
@@ -38,11 +41,18 @@ class Lukas():
     def animation(self):
         if not(self.timer_animation):
             if (not self.falling): self.pozice = (self.pozice + 1)%2
+            self.animation_delay = self.default_animation_delays[1]
+            if (right_pressed):
+                self.animation_delay = self.default_animation_delays[2]
             self.timer_animation.set(self.animation_delay)
 
     def move(self):
         if not(self.timer_move):
             self.velocity += self.change
+            if (left_pressed):
+                self.x -= self.x_velocity
+            if (right_pressed):
+                self.x += self.x_velocity
             if (self.y + self.velocity > self.border):
                 self.y = self.border
                 self.velocity = 0
@@ -50,6 +60,8 @@ class Lukas():
             else:
                 self.y += self.velocity
             self.timer_move.set(self.move_delay)
+            return True
+        return False
 
     def print(self):
         self.animation()
@@ -64,8 +76,9 @@ class Obstacles():
     def __init__(self):
         self.obstacles = []
         self.timer = Timer()
-        self.delay = 40
-        self.velocity = -15
+        self.delay = 10
+        self.image = pygame.image.load("sprites/cactus_scaled.png")
+        self.velocity = -5
         self.timer.set(self.delay)
 
     def add_random(self):
@@ -73,8 +86,8 @@ class Obstacles():
         if (not len(self.obstacles)):
             x = 800
         else:
-            x = self.obstacles[-1].x + choice([200, 400, 600])
-        self.obstacles.append(Obstacle(x, y))
+            x = self.obstacles[-1].x + choice([300, 400, 600])
+        self.obstacles.append(Obstacle(x, y, self.image))
 
     def move(self):
         if not(self.timer):
@@ -84,16 +97,18 @@ class Obstacles():
                     del self.obstacles[i]
                     self.add_random()
             self.timer.set(self.delay)
+            return True
+        return False
 
     def print(self):
         for obstacle in self.obstacles:
             screen.blit(obstacle.image, (obstacle.x, obstacle.y))
 
 class Obstacle():
-    def __init__(self, x, y):
+    def __init__(self, x, y, img):
         self.x = x
         self.y = y
-        self.image = pygame.image.load("sprites/cactus_scaled.png")
+        self.image = img
 
 
 def play_menu_music():
@@ -110,14 +125,8 @@ def play_battle_music():
     pygame.mixer.music.load("sprites/overworld_theme.mp3")
     pygame.mixer.music.play(-1)
 
-def gameLoop():
-    screen.fill((0, 0, 0))
-    screen.blit(background, (0, 0))
-    lukas.move()
-    obstacles.move()
-    lukas.print()
-    obstacles.print()
-
+pygame.init()
+screen = pygame.display.set_mode((800, 800))
 background_menu = pygame_menu.baseimage.BaseImage("sprites/angel_face_logo.png")
 
 icon = pygame.image.load("sprites/lukas_hlava.png")
@@ -126,11 +135,13 @@ y_border = 370
 lukas = Lukas(100, y_border)
 obstacles = Obstacles()
 for i in range(5): obstacles.add_random()
-pygame.init()
 running = True
-screen = pygame.display.set_mode((800, 800))
-background = pygame.image.load("sprites/background_hogwarts03.png").convert()
+space_pressed = False
+left_pressed = False
+right_pressed = False
+background = pygame.image.load("sprites/background_hogwarts03.png")
 pygame.display.set_caption("#knotakjede")
+clock = pygame.time.Clock()
 mytheme = pygame_menu.Theme(background_color=(0, 0, 0, 0), # transparent background
                 title_background_color=(120, 47, 126),
                 title_font_shadow=True,
@@ -146,14 +157,30 @@ menu.add.button('leave', pygame_menu.events.EXIT)
 
 play_menu_music()
 while True:
+    clock.tick(50)
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             exit()
         if event.type == pygame.KEYDOWN:
             if (event.key == pygame.K_SPACE):
-                if not(lukas.falling):
-                    lukas.jump()
+                space_pressed = True
+            if (event.key == pygame.K_LEFT):
+                left_pressed = True
+            if (event.key == pygame.K_RIGHT):
+                right_pressed = True
+        if event.type == pygame.KEYUP:
+            if (event.key == pygame.K_SPACE):
+                space_pressed = False
+            if (event.key == pygame.K_LEFT):
+                left_pressed = False
+            if (event.key == pygame.K_RIGHT):
+                right_pressed = False
+    if (space_pressed):
+        lukas.jump()
+
+
+
 
     if not game:
         menu.draw(screen)
@@ -161,7 +188,13 @@ while True:
             pygame.display.update()
             menu.draw(screen)
             screen.fill((0, 0, 0))
+        pygame.display.update()
     else:
-        gameLoop()
-    pygame.display.update()
+        lukas.move()
+        obstacles.move()
+        screen.blit(background, (0, 0))
+        lukas.print()
+        obstacles.print()
+        pygame.display.update()
+
 
