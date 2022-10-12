89f4b6c4b461efdf0e78ac6877fea34f13e98f1a
david <kolard@jirovcovka.net>
2022-10-12 23:00:20 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def set_level():
         level += 1
 
 def play_menu_music():
-    pygame.mixer.music.load(f"""sprites/menu_theme{"2" if (randint(0, 4) == 0) else ""}.mp3""")
+    pygame.mixer.music.load(f"""sprites/menu_theme{"2" if (randint(0, 8) == 0) else ""}.mp3""")
     pygame.mixer.music.play(-1)
 
 def start_the_game():
