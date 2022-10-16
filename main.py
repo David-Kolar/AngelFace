5ceb556728cd60093fb7c282a0a3d7c20ca68e03
david <kolard@jirovcovka.net>
2022-10-16 15:22:36 +0200

    Initial Commit

--- main.py
+++ main.py
@@ -66,8 +66,8 @@ class Lukas():
         self.pozice = 0
         self.change = 1
         self.jump_velocity = -20
-        img1 = pygame.image.load("sprites/lukas_tycka01.png")
-        img2 = pygame.image.load("sprites/lukas_tycka02.png")
+        img1 = pygame.image.load("sprites/grafika/lukas_tycka01.png")
+        img2 = pygame.image.load("sprites/grafika/lukas_tycka02.png")
         self.animations = [pygame.transform.scale(img1, (img1.get_width()*3, img1.get_height()*3)), pygame.transform.scale(img2, (img2.get_width()*3, img2.get_height()*3))]
 
     def animation(self):
@@ -110,7 +110,7 @@ class Obstacles():
         self.obstacles = []
         self.timer = Timer()
         self.delay = 10
-        self.image = pygame.image.load("sprites/cactus_scaled.png")
+        self.image = pygame.image.load("sprites/grafika/cactus_scaled.png")
         self.velocities = [-5, -7, -8, -10, -12, -14]
         self.timer.set(self.delay)
 
@@ -152,7 +152,7 @@ def set_level():
         level += 1
 
 def play_menu_music():
-    pygame.mixer.music.load(f"""sprites/menu_theme{"2" if (randint(0, 20) == 0) else ""}.mp3""")
+    pygame.mixer.music.load(f"""sprites/hudba/menu_theme{"2" if (randint(0, 20) == 0) else ""}.mp3""")
     pygame.mixer.music.play(-1)
 
 def start_the_game():
@@ -171,21 +171,21 @@ def start_the_game():
     pygame.display.update()
 
 def load_company_intro():
-    image = pygame.image.load("sprites/knot_foundation.png")
-    sound = pygame.mixer.Sound("sprites/company_sound.mp3")
+    image = pygame.image.load("sprites/grafika/knot_foundation.png")
+    sound = pygame.mixer.Sound("sprites/zvuky/company_sound.mp3")
     pygame.mixer.Sound.play(sound)
     screen.blit(image, (0, 0))
     pygame.display.update()
     pygame.time.wait(11000)
 
 def load_cia_warning():
-    image = pygame.image.load("sprites/cia.png")
+    image = pygame.image.load("sprites/grafika/cia.png")
     screen.blit(image, (0, 0))
     pygame.display.update()
     pygame.time.wait(4000)
 
 def play_battle_music():
-    pygame.mixer.music.load("sprites/overworld_theme.mp3")
+    pygame.mixer.music.load("sprites/hudba/overworld_theme.mp3")
     pygame.mixer.music.play(-1)
 
 def set_one_player_menu():
@@ -211,14 +211,14 @@ def set_menu():
 class Zvuky():
     def __init__(self, audio):
         self.audio = audio
-        self.skok = pygame.mixer.Sound("sprites/Mario Jump - Gaming Sound Effect (HD)20150625.mp3")
+        self.skok = pygame.mixer.Sound("sprites/zvuky/Mario Jump - Gaming Sound Effect (HD)20150625.mp3")
         self.skok.set_volume(0.04)
-        self.smrt = pygame.mixer.Sound("sprites/death.mp3")
-        self.lets_go = pygame.mixer.Sound("sprites/Lets go.mp3")
-        self.wow = pygame.mixer.Sound("sprites/Wow.mp3")
-        self.amazing = pygame.mixer.Sound("sprites/Amazing.mp3")
-        self.sheesh = pygame.mixer.Sound("sprites/Sheesh.mp3")
-        self.select = pygame.mixer.Sound("sprites/menu_select_sound.mp3")
+        self.smrt = pygame.mixer.Sound("sprites/zvuky/death.mp3")
+        self.lets_go = pygame.mixer.Sound("sprites/zvuky/Lets go.mp3")
+        self.wow = pygame.mixer.Sound("sprites/zvuky/Wow.mp3")
+        self.amazing = pygame.mixer.Sound("sprites/zvuky/Amazing.mp3")
+        self.sheesh = pygame.mixer.Sound("sprites/zvuky/Sheesh.mp3")
+        self.select = pygame.mixer.Sound("sprites/zvuky/menu_select_sound.mp3")
     def play(self, zvuk):
         if (self.audio): pygame.mixer.Sound.play(zvuk)
 
@@ -230,18 +230,18 @@ beggining = True
 
 
 screen = pygame.display.set_mode((800, 800))
-icon = pygame.image.load("sprites/lukas_hlava.png")
+icon = pygame.image.load("sprites/grafika/lukas_hlava.png")
 pygame.display.set_icon(icon)
 y_border = 370
 running = True
 space_pressed = False
 left_pressed = False
 right_pressed = False
-background = pygame.image.load("sprites/background_hogwarts05.png")
+background = pygame.image.load("sprites/grafika/background_hogwarts05.png")
 pygame.display.set_caption("#knotakjede")
 clock = pygame.time.Clock()
 ###################### Herni menu #################################################
-background_menu = pygame.image.load("sprites/background_hogwarts_new.png")
+background_menu = pygame.image.load("sprites/grafika/background_hogwarts_new.png")
 mytheme = pygame_menu.Theme(background_color=(0, 0, 0, 0), # transparent background
                 title_font_shadow=True,
                 widget_padding=25,
@@ -255,7 +255,7 @@ menu.add.button('two players', set_two_players_menu)
 menu.add.button('leave', pygame_menu.events.EXIT)
 ###################################################################################
 ################### Game Over Menu ################################################
-game_over_background_image = pygame.image.load("sprites/game_over.png")
+game_over_background_image = pygame.image.load("sprites/grafika/game_over.png")
 game_over = pygame_menu.Menu("", 800, 800, theme=mytheme)
 game_over.add.button("Play", start_the_game)
 game_over.add.button("Back to menu", set_menu)
@@ -274,7 +274,7 @@ two_players.add.button('play', start_the_game)
 two_players.add.button('back', set_menu)
 ###################################################################################
 active_menu = menu
-font = pygame.font.Font(abspath("sprites/pixel_font.ttf"), 32)
+font = pygame.font.Font(abspath("sprites/pismo/pixel_font.ttf"), 32)
 levels = [100, 200, 300, 400, 500, float("inf")]
 predchozi_score = 0
 if (config["cia"]):
